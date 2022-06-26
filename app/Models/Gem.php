7dd48206740f51578a722cc7a1f2f47a02c6470c
Amir Sasani <sasani.amir.1998@gmail.com>
2022-06-26 12:28:20 +0430

    add gem and gem-transaction migration with their models

---
+++ app/Models/Gem.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Gem extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'gem'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
